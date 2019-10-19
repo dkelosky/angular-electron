a366fba90101945aec1aac40acf49b2ad14ca1a6
Dan Kelosky <dkelosky@gmail.com>
2019-10-18 21:31:28 -0400

    first case of api working

--- src/app/core/services/electron/electron.service.ts
+++ src/app/core/services/electron/electron.service.ts
@@ -6,6 +6,9 @@ import { ipcRenderer, webFrame, remote } from 'electron';
 import * as childProcess from 'child_process';
 import * as fs from 'fs';
 
+import { GetJobs, IJob } from "@zowe/cli";
+import { Session, ISession, Logger, LoggingConfigurer } from "@zowe/imperative";
+
 @Injectable({
   providedIn: 'root'
 })
@@ -15,6 +18,8 @@ export class ElectronService {
   remote: typeof remote;
   childProcess: typeof childProcess;
   fs: typeof fs;
+  session: typeof Session;
+  getJobs: typeof GetJobs;
 
   get isElectron() {
     return window && window.process && window.process.type;
@@ -29,6 +34,26 @@ export class ElectronService {
 
       this.childProcess = window.require('child_process');
       this.fs = window.require('fs');
+
+      this.session = window.require('@zowe/imperative').Session;
+      this.getJobs = window.require('@zowe/cli').GetJobs;
+
+      const connectionInfo: ISession = {
+        hostname: "usildamd.lvn.broadcom.net",
+        user: "kelda16",
+        password: "pass",
+        type: "basic",
+        rejectUnauthorized: false
+      };
+
+      // create a session & get jobs
+      const session: Session = new this.session(connectionInfo);
+
+      this.getJobs.getJobs(session).then((jobs) => {
+        jobs.forEach((job) => {
+          console.log(job.jobname);
+        });
+      });
     }
   }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GetJobs, IJob } from "@zowe/cli";
+import { Session, ISession, Logger, LoggingConfigurer } from "@zowe/imperative";
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  jobs: IJob[];
+
   constructor() { }
 
-  ngOnInit() {
+  async ngOnInit() {
+
+  //   // init dummy logger for imperative
+  //   Logger.initLogger(LoggingConfigurer.configureLogger(".sample", { name: "sample" }));
+
+  //   // connection info (host, user, password, etc...)
+  //   const connectionInfo: ISession = {
+  //     hostname: "usildamd.lvn.broadcom.net",
+  //     user: "kelda16",
+  //     password: "ghjk5678",
+  //     type: "basic",
+  //     rejectUnauthorized: false
+  //   };
+
+  //   // create a session & get jobs
+  //   const session: Session = new Session(connectionInfo);
+  //   this.jobs = await GetJobs.getJobs(session);
   }
 
 }
