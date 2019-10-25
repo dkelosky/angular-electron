e82c19fab6a9b493cab1e87a45419c1ca8c0b93a
Dan Kelosky <dkelosky@gmail.com>
2019-10-25 12:29:10 -0400

    proxy test

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { IJob } from "@zowe/cli";
 import { JobService } from '../core/services/jobs/job.service';
+import { HttpClientModule, HttpClient, HttpHeaders } from '@angular/common/http';
+// import { Htt}
 
 @Component({
   selector: 'app-home',
@@ -12,11 +14,17 @@ export class HomeComponent implements OnInit {
 
   jobs: IJob[];
 
-  constructor(private js: JobService) { }
+  constructor(private js: JobService, private http: HttpClient) { }
 
   async ngOnInit() {
 
     this.jobs = await this.js.getAllJobs();
+    const headers = new HttpHeaders();
+    // headers.set("Access-Control-Allow-Origin", "*");
+    // this.http.get("localhost:4200/jobs", {headers}).subscribe((data) => {
+      this.http.get("http://localhost:4200/jobs").subscribe((data) => {
+      console.log(data)
+    })
   }
 
 }
